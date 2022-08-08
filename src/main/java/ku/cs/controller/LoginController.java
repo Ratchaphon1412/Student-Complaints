fb8f6508cadbc140af83e93eef799687e9b5ec13
Ratchaphon Hinsui <ratchaphonhinsui@Ratchaphons-MacBook-Pro.local>
2022-08-08 20:37:57 +0700

    edit loginView.fxml and create method initialize for set Image

--- src/main/java/ku/cs/controller/LoginController.java
+++ src/main/java/ku/cs/controller/LoginController.java
@@ -1,5 +1,25 @@
 package ku.cs.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
 public class LoginController {
+
+    @FXML
+    private ImageView logoKU;
+
+    @FXML
+    private VBox pictureLoginBackground;
+    @FXML
+    private void initialize(){
+        String logoKUPic = getClass().getResource("/ku/cs/assets/images/LogoKU.png").toExternalForm();
+        logoKU.setImage(new Image(logoKUPic));
+
+
+    }
+
+
 
 }
