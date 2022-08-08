61d9c98102daa3e32929f3f4ea6e481ca01e1afa
poomffi <poomffi@hotmail.com>
2022-08-08 19:36:26 +0700

    Create readFile method

---
+++ src/main/java/ku/cs/models/Service/DataBase.java
@@ -0,0 +1,47 @@
+package ku.cs.models.Service;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class DataBase {
+
+    private boolean readFile(String name, String password, String file){
+        String line = "";
+        ArrayList<String[]> bigListAdmin = new ArrayList();
+        String[] listAdmin;
+        BufferedReader adminDataBase = null;
+        boolean loginStage = false;
+        try {
+            adminDataBase = new BufferedReader(new FileReader(file));
+            while ((line = adminDataBase.readLine()) != null) {
+                listAdmin = line.split(",");
+                bigListAdmin.add(listAdmin);
+            }
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for(int i = 0 ; i < bigListAdmin.size(); i++){
+            if(bigListAdmin.get(i)[0].equals(name) && bigListAdmin.get(i)[1].equals(password)){
+                loginStage = true;
+                break;
+            }
+        }
+        if(loginStage){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+
+    }
+
+
+}
