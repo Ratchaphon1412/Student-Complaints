7a419e19cd061ed4e2f0cb38c3d488a7a0fb7da5
poomffi <poomffi@hotmail.com>
2022-09-02 01:00:04 +0700

    draft admin models

---
+++ src/main/java/ku/cs/models/Admin.java
@@ -0,0 +1,14 @@
+package ku.cs.models;
+
+public class Admin {
+
+    public boolean banUser(Object user){
+//        user.setBan(user.getBan() ? false:true);
+        return true;
+    }
+    public Object changeStuffAgency(Object name, Object newName){
+        return null;
+    }
+
+
+}
