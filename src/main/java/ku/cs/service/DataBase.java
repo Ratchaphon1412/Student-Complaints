fd46d1d6f43f97fcffd5f535575bf277979c8c24
poomffi <poomffi@hotmail.com>
2022-09-03 00:01:04 +0700

    edit method readfile in database

--- src/main/java/ku/cs/service/DataBase.java
+++ src/main/java/ku/cs/service/DataBase.java
@@ -1,11 +1,13 @@
 package ku.cs.service;
 import ku.cs.models.user.User;
 import ku.cs.models.stuff.Stuff;
+
+import java.io.*;
 import java.util.LinkedHashMap;
 public class DataBase<DataObject> implements DynamicDatabase<DataObject> {
-    private LinkedHashMap<String,String> accountList;
-    private LinkedHashMap<String,String> reportList;
-    private LinkedHashMap<String,String> stuffList;
+    private LinkedHashMap<String, LinkedHashMap<String,String>> accountList;
+    private LinkedHashMap<String, LinkedHashMap<String,String>> reportList;
+    private LinkedHashMap<String, LinkedHashMap<String,String>> stuffList;
 
     public DataBase(){
         readFile();
@@ -17,7 +19,33 @@ public class DataBase<DataObject> implements DynamicDatabase<DataObject> {
 //
 //    }
     private void readFile(){
-
+        String filePath = "database" + File.separator + "account.csv";
+        File file = new File(filePath);
+        BufferedReader buffer = null;
+        FileReader reader = null;
+        try {
+            String line = "";
+            reader = new FileReader(file);
+            buffer = new BufferedReader(reader);
+            while ((line = buffer.readLine()) != null) {
+                System.out.println(line);
+                String[] data = line.split(",");
+                MemberCard card = new MemberCard(data[0].trim() , data[1].trim() , Integer.parseInt(data[2].trim()));
+                list.addCard(card);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            try {
+                buffer.close();
+                reader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
     private void writeFile(){
 
