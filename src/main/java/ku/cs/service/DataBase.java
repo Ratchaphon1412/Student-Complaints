6801d0eee11a47107122d5dd47caca6bfe304925
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2022-09-02 21:18:00 +0700

    create database csv

--- src/main/java/ku/cs/service/DataBase.java
+++ src/main/java/ku/cs/service/DataBase.java
@@ -5,6 +5,9 @@ import ku.cs.models.stuff.Stuff;
 public class DataBase<DataObject> implements DynamicDatabase<DataObject> {
 
 
+    public DataBase(){
+
+    }
 
 
 //    public DataObject login(String userName ,String passWord){
