ab1130ab03aca6b472280b78f50df0b2c21a620e
Gug Jirapat <jirapat.sur@ku.th>
2022-09-22 11:47:43 +0700

    link adminDashbaord to Setting

--- src/main/java/ku/cs/controller/AdminController.java
+++ src/main/java/ku/cs/controller/AdminController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -186,6 +187,16 @@ public class AdminController {
             System.err.println(e);
         }
     }
+
+    @FXML
+    public void handleAdminSettingButton(MouseEvent mouseEvent) {
+        try {
+            ApplicationController.goTo("Setting",account);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
 
 
 
