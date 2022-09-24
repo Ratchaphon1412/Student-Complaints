10b947922273dd2c330743989d815b06278258f1
poomffi <poomffi@hotmail.com>
2022-09-24 19:59:33 +0700

    edit BanuserOrUnban show Chart and percent

--- src/main/java/ku/cs/controller/admin/AdminUserBanedListController.java
+++ src/main/java/ku/cs/controller/admin/AdminUserBanedListController.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import ku.cs.controller.components.AdminUserBanListController;
@@ -26,24 +27,36 @@ import java.io.IOException;
 
 public class AdminUserBanedListController {
     @FXML
-    public GridPane gridPaneList;
-    @FXML
     private GridPane adminpage;
-    @FXML
-    private GridPane listPostReportGrid;
 
     @FXML
     private PieChart countBanned;
+
+    @FXML
+    private PieChart countReportBan;
+
+    @FXML
+    private GridPane gridPaneList;
+
     @FXML
     private Circle imageAccount;
+
+    @FXML
+    private GridPane listPostReportGrid;
+
+
+    @FXML
+    private Label lordUserUnban1;
+
+    @FXML
+    private Label lordUserUnban2;
+
     @FXML
     private Label roleAccountLabel;
 
     @FXML
     private Label userNameAccountLabel;
 
-    @FXML
-    private PieChart countReportBan;
 
     private ProcessData processData;
     private UserList userList;
@@ -58,9 +71,6 @@ public class AdminUserBanedListController {
 
         roleAccountLabel.setText(account.getRole());
         userNameAccountLabel.setText(account.getUserName());
-        String fs = File.separator;
-        File desDir = new File("image" + fs +  "accounts" + fs+ account.getPathPicture());
-        imageAccount.setFill(new ImagePattern(new Image(desDir.toURI().toString(),800, 0 ,true,true)));
 
 
         //load NavBar
@@ -68,11 +78,13 @@ public class AdminUserBanedListController {
         GridPane navbar = (GridPane) fxmlLoader.load(getClass().getResource("/ku/cs/components/navBarAdmin.fxml"));
         adminpage.add(navbar,0,0);
 
-
         refetch();
     }
 
     private void refetch() throws IOException {
+
+        File desDir = new File("image"+System.getProperty("file.separator")+"accounts"+System.getProperty("file.separator")+account.getPathPicture());
+        imageAccount.setFill(new ImagePattern(new Image(desDir.toURI().toString(),800, 0 ,true,true)));
 
         processData = new ProcessData<>();
         userList = processData.getUserList();
@@ -94,7 +106,6 @@ public class AdminUserBanedListController {
             AnchorPane listUser = (AnchorPane) fxmlLoader.load();
             AdminUserBanListController adminUserBanListController = fxmlLoader.getController();
             adminUserBanListController.setData(userBan,account,banAndUnBan);
-            System.out.println(userBan);
 
             gridPaneList.add(listUser,0,count++);
             GridPane.setMargin(listUser, new Insets(0,0,5,0));
@@ -118,24 +129,32 @@ public class AdminUserBanedListController {
                 new PieChart.Data("All user Banned",userList.getUserBanList().size()),
                 new PieChart.Data("All user",userList.getUserList().size())
         );
+        int percentBanned = (userList.getUserBanList().size() / userList.getUserList().size());
         countBanned.setClockwise(true);
         countBanned.setLabelsVisible(true);
         countBanned.setLabelLineLength(50);
         countBanned.setStartAngle(180);
         countBanned.setData(pieChartBanned);
-//        countBanned.getData().clear();
 
         ObservableList<PieChart.Data> pieChartReportBan = FXCollections.observableArrayList(
                 new PieChart.Data("All user Banned",userList.getUserRequestBan().size()),
                 new PieChart.Data("All user",userList.getUserList().size())
         );
+        int percentReport = (userList.getUserRequestBan().size() / userList.getUserList().size());
         countReportBan.setClockwise(true);
         countReportBan.setLabelsVisible(true);
         countReportBan.setLabelLineLength(50);
         countReportBan.setStartAngle(180);
         countReportBan.setData(pieChartReportBan);
+//        lordUserUnban1.setText(percentBanned);
+//        lordUserUnban2.setText(percentReport);
+        double a,b,c;
+        a = userList.getUserBanList().size();
+        b = userList.getUserRequestBan().size();
+        c = userList.getUserList().size();
 
-
+        lordUserUnban1.setText(String.valueOf(Math.ceil(a/c*100)));
+        lordUserUnban2.setText(String.valueOf(Math.ceil(b/c*100)));
     }
 
 
