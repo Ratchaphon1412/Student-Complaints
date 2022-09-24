0d6aae75787a8f36b4348e5976612d10cd9505bc
root <root@DESKTOP-RS2UKNH.localdomain>
2022-09-24 19:50:45 +0700

    Add allProblemReportView.fxml

--- src/main/java/ku/cs/Application.java
+++ src/main/java/ku/cs/Application.java
@@ -21,7 +21,7 @@ public class Application extends javafx.application.Application {
         //route config
         configRoute();
 
-        ApplicationController.goTo("Login");
+        ApplicationController.goTo("Agency");
     }
 
 
