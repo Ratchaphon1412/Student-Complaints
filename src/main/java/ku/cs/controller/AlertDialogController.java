c9403b5de362d06729c6762a1945d7c0e9779a0a
Gug Jirapat <jirapat.sur@ku.th>
2022-09-25 16:48:12 +0700

    alert can close now

--- src/main/java/ku/cs/controller/AlertDialogController.java
+++ src/main/java/ku/cs/controller/AlertDialogController.java
@@ -1,9 +1,12 @@
 package ku.cs.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import ku.cs.ApplicationController;
 
 import java.io.IOException;
@@ -16,6 +19,9 @@ public class AlertDialogController {
     @FXML
     private Label textStatus;
 
+    @FXML
+    private AnchorPane anchorPane;
+
 
     @FXML
     private void initialize() throws IOException{
@@ -24,6 +30,9 @@ public class AlertDialogController {
             String pathPicture = getClass().getResource("/ku/cs/assets/images/error-icon-4.png").toExternalForm();
             status.setImage(new Image(pathPicture));
             textStatus.setText(text);
+
+        HBox buttonWindows = (HBox) FXMLLoader.load(getClass().getResource("/ku/cs/components/buttonWindows.fxml"));
+        anchorPane.getChildren().add(buttonWindows);
 
 
     }
