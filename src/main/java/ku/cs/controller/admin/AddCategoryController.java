e78f11c6929f548d8a65ead8b2140d3b2fa64359
oOTEEOo <thanadon.kr@ku.th>
2022-10-01 17:29:20 +0700

    commit for pull

--- src/main/java/ku/cs/controller/admin/AddCategoryController.java
+++ src/main/java/ku/cs/controller/admin/AddCategoryController.java
@@ -3,8 +3,13 @@ package ku.cs.controller.admin;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import ku.cs.ApplicationController;
+import ku.cs.controller.components.StaffListAgencyController;
 import ku.cs.service.DataBase;
 import ku.cs.service.ProcessData;
 
@@ -13,7 +18,26 @@ import java.io.IOException;
 public class AddCategoryController {
     @FXML
     private TextField addCatagoryField;
+    @FXML
+    private GridPane girdPaneAddCategory;
     private ProcessData processData;
+    private FXMLLoader fxmlLoader;
+
+
+    @FXML
+    public void initialize() throws IOException {
+        processData = new ProcessData();
+        initializeCategory();
+
+    }
+    @FXML void initializeCategory() throws IOException {
+        fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/ku/cs/components/categoryList.fxml"));
+        AnchorPane categoryComponant = (AnchorPane) fxmlLoader.load();
+        AddCategoryController addCategoryController = fxmlLoader.getController();
+
+        GridPane.setMargin(categoryComponant, new Insets(0,0,5,0));
+    }
 
     @FXML
     void closeButton(ActionEvent event) {
@@ -26,8 +50,8 @@ public class AddCategoryController {
 
     @FXML
     void applyButton(ActionEvent event) throws IOException {
-        addCatagoryField.getText();
-        System.out.println("get category");
+        String category = addCatagoryField.getText();
+        processData.addCategory(category);
     }
 
 
--- src/main/java/ku/cs/controller/components/CategoryListController.java
+++ src/main/java/ku/cs/controller/components/CategoryListController.java
@@ -2,11 +2,16 @@ package ku.cs.controller.components;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import ku.cs.controller.admin.AddCategoryController;
+
 
 public class CategoryListController {
 
     @FXML
     private Label categoryLabel;
+
+    @FXML
+    public void initialize(){
+        categoryLabel.setText();
+    }
 
 }
