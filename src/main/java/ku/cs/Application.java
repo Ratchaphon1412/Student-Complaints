fde625e5e958cbd58a3aa8ac7eec40b6f0bbe58c
oOTEEOo <thanadon.kr@ku.th>
2022-10-06 21:39:42 +0700

    CHange createPostview to user directory

--- src/main/java/ku/cs/Application.java
+++ src/main/java/ku/cs/Application.java
@@ -71,7 +71,7 @@ public class Application extends javafx.application.Application {
         ApplicationController.when("Report",pathResource+"user/detailProblemView.fxml",1000,580);
         ApplicationController.when("UserPostsProfile",pathResource+"user/userPostsView.fxml",1000,580);
         ApplicationController.when("UserSetting",pathResource+"user/userSettingView.fxml",1000, 580);
-        ApplicationController.when("Post",pathResource+"createPostView.fxml",1000,580);
+        ApplicationController.when("Post",pathResource+"user/createPostView.fxml",1000,580);
 
         //Staff zone
         ApplicationController.when("Staff",pathResource+"staff/staffProcessProblem.fxml",1000,580);
